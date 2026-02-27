b9787dc66913d9ac30b3d649a03544d5ea5320d2
RS256 <143985949+RS-256@users.noreply.github.com>
2026-02-28 04:17:24 +0900

    Why on earth did I leave this here without using it?

--- src/main/java/com/rs256/infinote/client/InfinoteClient.java
+++ src/main/java/com/rs256/infinote/client/InfinoteClient.java
@@ -3,12 +3,8 @@ package com.rs256.infinote.client;
 import com.rs256.infinote.Infinote;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.util.RandomSource;
 
 public class InfinoteClient implements ClientModInitializer {
-
-    private static final RandomSource RNG = RandomSource.create();
-
     @Override
     public void onInitializeClient() {
     }
